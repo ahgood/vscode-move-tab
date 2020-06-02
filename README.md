b27fa37a6ed410d3ac034a698a73dc3dfd61dce9
KK Yang <kk.yang@redspace.com>
2020-06-02 20:05:51 -0300

    Add more functions, icon and screenshot

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-# Move Tab to First Position
+# Move tab to first/last/left/right position without using mouse
 
 ## How to use
 
-- macOS: `CMD + Shift + p` -> `Move tab`
-- Windows: `ctrl + shift + p` -> `Move tab`
+macOS: `command + shift + p` → `movetab`
+Windows: `ctrl + shift + p` → `movetab`
+
+- Move Tab: To the first position
+- Move Tab: To the last position
+- Move Tab: To the left
+- Move Tab: To the right
+
+## Screenshot
+
+![Screenshot](screenshot.png)
--- extension.js
+++ extension.js
@@ -1,20 +1,21 @@
 const vscode = require('vscode');
 
+function registerCommand(command, parameter) {
+	return vscode.commands.registerCommand(`movetab.${command}`, function () {
+		vscode.commands.executeCommand('moveActiveEditor', parameter);
+	});
+}
+
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	let disposable = vscode.commands.registerCommand('movetab.moveTab', function () {
-		vscode.commands.executeCommand('moveActiveEditor', { to: 'first' });
-	});
+	const toFirst = registerCommand('moveTabToFirst', { to: 'first' });
+	const toLast = registerCommand('moveTabToLast', { to: 'last' });
+	const toLeft = registerCommand('moveTabToLeft', { to: 'left', value: 1 });
+	const toRight = registerCommand('moveTabToRight', { to: 'right', value: 1 });
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(toFirst, toLast, toLeft, toRight);
 }
+
 exports.activate = activate;
-
-function deactivate() {}
-
-module.exports = {
-	activate,
-	deactivate
-}
